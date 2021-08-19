ef58cc6c4682f4cbfb3a25ef2125949ef6bbb64c
Mariia Trusova <tmv_2008@ukr.net>
2021-08-19 18:21:43 +0300

    init+

---
+++ src/components/user/User.js
@@ -0,0 +1,5 @@
+export default function User({item: userItem}) {
+  return (
+      <div> {userItem.id} {userItem.name}</div>
+  );
+}
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,11 @@
 import {usersList} from "../../data-file";
+import User from "../user/User";
 
 export default function Users() {
   return (
     <div>
         {
-          usersList.map(userItem => <div> {userItem.id} {userItem.name}</div>)
+          usersList.map(userItem => <User key={userItem.id} item={userItem}/>)
         }
     </div>
   );
